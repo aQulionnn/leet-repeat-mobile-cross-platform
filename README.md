6f3b5073c8a880affdca4cc6a4753a1104edbc8c
bekasker <2004bek.asker@mail.ru>
2026-01-07 23:17:54 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# leet_repeat_mobile_cross_platform
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/leet_repeat_mobile_cross_platform/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.leet_repeat_mobile_cross_platform
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
